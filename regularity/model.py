079b780feaf9327450afef53d2c069fca51ba354
Tim Johnson <generic.tjohnson@gmail.com>
2011-11-16 17:16:47 -0500

    Changed the model to do unions and truncations.

--- regularity/model.py
+++ regularity/model.py
@@ -2,30 +2,103 @@ import datetime
 import pymongo
 import uuid
 
+def union(*activities):
+    pass
+
+
 class Model(object):
 
-    CONTIGUITY_THRESHOLD = 10 # seconds
+    CONTIGUITY_THRESHOLD = 5 # seconds
 
     def __init__(self, host='localhost'):
         connection = pymongo.Connection(host)
         self.db = connection.regularity
         self.uuid = uuid.uuid4().hex
 
-    def previous(self, timeline, start):
-        criteria = {
-            'session' : self.uuid,
+    def overlapping(self, timeline, start, end, buffer_=None, **kwargs):
+
+        if buffer_ is None:
+            buffer_ = self.CONTIGUITY_THRESHOLD
+        buffer_ = datetime.timedelta(seconds=buffer_)
+
+        start = start - buffer_
+        end = end + buffer_
+
+        criteria = kwargs
+        criteria.update({
+#            'session' : self.uuid,
             'timeline' : timeline,
-            'end' : { '$gt' : start - datetime.timedelta(seconds=self.CONTIGUITY_THRESHOLD), '$lte' : start },
-        }
+            '$or' : [
+                { 'start' : { '$lte' : start }, 'end' : { '$gte' : end } },
+                { 'start' : { '$gte' : start }, 'start' : { '$lte' : end } },
+                { 'end' : { '$gte' : start }, 'end' : { '$lte' : end } },
+            ]
+        })
         query = self.db.timelines.find(criteria)
         query = query.sort( 'end', -1)
         query = query.limit(1)
-        previous = tuple(query)
+        overlapping = tuple(query)
 
-        if previous:
-            return previous[0]
+        return overlapping
 
-        return None
+    def union(self, timeline, activity, start, end):
+        '''Union this activity with any activities on the same timeline that 
+           overlap and are the same activity.'''
+
+        extra_criteria = {
+            'activity' : activity
+        }
+        overlapping_activities = self.overlapping(timeline, start, end, **extra_criteria)
+
+        if overlapping_activities:
+            start = min(start, *(a['start'] for a in overlapping_activities))
+            end = max(end, *(a['end'] for a in overlapping_activities))
+
+            for a in overlapping_activities:
+                self.db.timelines.remove(a)
+
+        data = dict(
+            session=self.uuid,
+            timeline=timeline,
+            activity=activity,
+            start=start,
+            end=end,
+        )
+
+        self.db.timelines.save(data)
+        return data
+
+    def truncate(self, timeline, activity, start, end):
+        '''Truncate all activities on the same timeline that overlap and are
+           not the same activity.'''
+
+        extra_criteria = {
+            'activity' : { '$ne' : activity },
+        }
+        overlapping_activities = self.overlapping(timeline, start, end, **extra_criteria)
+
+        for activity in overlapping_activities:
+            if activity['end'] < end:
+                activity['end'] = start
+
+                self.db.timelines.save(activity)
+
+            elif activity['start'] > start:
+                activity['start'] = end
+
+                self.db.timelines.save(activity)
+
+            else:
+                # split the activity into two
+                activity1 = dict(activity)
+                activity2 = dict(activity)
+
+                activity1['end'] = start
+                activity2['start'] = end
+
+                self.db.timelines.remove(activity)
+                self.db.timelines.save(activity1)
+                self.db.timelines.save(activity2)
 
     def log(self, timeline, activity, start=None, end=None):
         if start is None:
@@ -34,23 +107,9 @@ class Model(object):
         if end is None:
             end = start
 
-        previous = self.previous(timeline, start)
-        #todo see if there are overlaps between this and a possible next
+        data = self.union(timeline, activity, start, end)
+        self.truncate(timeline, activity, data['start'], data['end'])
 
-        if previous and previous['activity'] == activity:
-            previous['end'] = start
-            self.db.timelines.save(previous)
-        else:
-            if previous:
-                start = previous['end']
-
-            data = dict(
-                session=self.uuid,
-                timeline=timeline,
-                activity=activity,
-                start=start,
-                end=end)
-            self.db.timelines.save(data)
 
     def update(self, timeline, activity):
         end = datetime.datetime.utcnow()
