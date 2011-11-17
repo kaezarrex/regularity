55b88ec0800ea3276e72a951e7b4197d54fd865a
Tim Johnson <generic.tjohnson@gmail.com>
2011-11-16 23:05:10 -0500

    Fixed the broken overlapping query, and added documentation.

--- regularity/model.py
+++ regularity/model.py
@@ -11,11 +11,25 @@ class Model(object):
     CONTIGUITY_THRESHOLD = 5 # seconds
 
     def __init__(self, host='localhost'):
+        '''Create a connection to mongoDB
+
+           @param host : optional, str, the host of the mongoDB server'''
+
         connection = pymongo.Connection(host)
         self.db = connection.regularity
         self.uuid = uuid.uuid4().hex
 
     def overlapping(self, timeline, start, end, buffer_=None, **kwargs):
+        '''Return timeline events that overlap with the time denoted by start
+           and end
+           
+           @param timeline : str, the timeline to scan for overlapping activities
+           @param start : datetime, the start time of the activity
+           @param end : datetime, the end time of the activity
+           @param buffer : optional, int, the number of seconds to buffer out
+                           the time range, useful for catching events that 
+                           barely don't overlap, defaults to 5 seconds
+           @param kwargs : optional, additional criteria for the query'''
 
         if buffer_ is None:
             buffer_ = self.CONTIGUITY_THRESHOLD
@@ -26,24 +40,25 @@ class Model(object):
 
         criteria = kwargs
         criteria.update({
-#            'session' : self.uuid,
             'timeline' : timeline,
-            '$or' : [
-                { 'start' : { '$lte' : start }, 'end' : { '$gte' : end } },
-                { 'start' : { '$gte' : start }, 'start' : { '$lte' : end } },
-                { 'end' : { '$gte' : start }, 'end' : { '$lte' : end } },
+            '$nor' : [
+                { 'start' : { '$gt' : end } },
+                { 'end' : { '$lt' : start } },
             ]
         })
         query = self.db.timelines.find(criteria)
-        query = query.sort( 'end', -1)
-        query = query.limit(1)
         overlapping = tuple(query)
 
         return overlapping
 
     def union(self, timeline, activity, start, end):
         '''Union this activity with any activities on the same timeline that 
-           overlap and are the same activity.'''
+           overlap and are the same activity.
+           
+           @param timeline : str, the timeline to scan for overlapping activities
+           @param activity : str, the name of the activity to match up
+           @param start : datetime, the start time of the activity
+           @param end : datetime, the end time of the activity'''
 
         extra_criteria = {
             'activity' : activity
@@ -70,37 +85,60 @@ class Model(object):
 
     def truncate(self, timeline, activity, start, end):
         '''Truncate all activities on the same timeline that overlap and are
-           not the same activity.'''
+           not the same activity.
+           
+           @param timeline : str, the timeline to scan for overlapping activities
+           @param activity : str, the name of the activity to match up
+           @param start : datetime, the start time of the activity
+           @param end : datetime, the end time of the activity'''
 
         extra_criteria = {
             'activity' : { '$ne' : activity },
         }
         overlapping_activities = self.overlapping(timeline, start, end, **extra_criteria)
 
-        for activity in overlapping_activities:
-            if activity['end'] < end:
-                activity['end'] = start
+        for a in overlapping_activities:
+            if a['end'] < end:
+                a['end'] = start
 
-                self.db.timelines.save(activity)
+                self.db.timelines.save(a)
 
-            elif activity['start'] > start:
-                activity['start'] = end
+            elif a['start'] > start:
+                a['start'] = end
 
-                self.db.timelines.save(activity)
+                self.db.timelines.save(a)
 
             else:
                 # split the activity into two
-                activity1 = dict(activity)
-                activity2 = dict(activity)
+                activity1 = dict(
+                    session=self.uuid,
+                    timeline=timeline,
+                    activity=a['activity'],
+                    start=a['start'],
+                    end=start
+                )
+                activity2 = dict(
+                    session=self.uuid,
+                    timeline=timeline,
+                    activity=a['activity'],
+                    start=end,
+                    end=a['end'],
+                )
 
-                activity1['end'] = start
-                activity2['start'] = end
-
-                self.db.timelines.remove(activity)
+                self.db.timelines.remove(a)
                 self.db.timelines.save(activity1)
                 self.db.timelines.save(activity2)
 
     def log(self, timeline, activity, start=None, end=None):
+        '''Log the occurence of an activity to the specified timeline.
+
+           @param timeline : str, name of the timeline
+           @param activity : str, name of the activity
+           @param start : optional, datetime, the start time of the activity,
+                          defaults to now
+           @param end : optional, datetime, the end time of the activity,
+                        defaults to start'''
+
         if start is None:
             start = datetime.datetime.utcnow()
 
@@ -112,6 +150,11 @@ class Model(object):
 
 
     def update(self, timeline, activity):
+        '''Log the continuance of an ongoing activity to the specified timeline
+
+           @param timeline : str, the name of the timeline
+           @param activity : str, the name of the activity'''
+
         end = datetime.datetime.utcnow()
         start = end - datetime.timedelta(seconds=self.CONTIGUITY_THRESHOLD)
         
