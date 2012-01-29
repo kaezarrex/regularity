ed9836e8fbc05ea55e9a88b212fcd3fe2673df30
Tim Johnson <generic.tjohnson@gmail.com>
2012-01-29 13:46:39 -0500

    Added a serializer for object ids

--- regularity/api/serializers.py
+++ regularity/api/serializers.py
@@ -1,7 +1,26 @@
 
 import datetime as _datetime
 
+from pymongo.objectid import ObjectId
+
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
+def object_id(o):
+    '''(De)serialize the object to/from ObjectID/str.
+
+       @param o : pymongo.objectid.ObjectId | str
+           the object to (de)serialize'''
+
+    if isinstance(o, basestring):
+        # deserialize to datetime
+        return ObjectId(o)
+
+    elif isinstance(o, ObjectId):
+        # serialize to string
+        return str(o)
+
+    raise ValueError('%s is not a string or datetime' % o)
+
 def datetime(o):
     '''(De)serialize the object to/from datetime/str.
 
@@ -16,5 +35,4 @@ def datetime(o):
         # serialize to string
         return o.strftime(DATETIME_FORMAT)
 
-    else:
-        raise ValueError('%s is not a string or datetime' % o)
+    raise ValueError('%s is not a string or datetime' % o)
