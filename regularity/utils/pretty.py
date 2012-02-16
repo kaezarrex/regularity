e12da7f4a6d6fa29ffbd40cf953632f68e513efc
Tim Johnson <generic.tjohnson@gmail.com>
2012-02-15 22:03:48 -0500

    Fixed pretty printing of durations.

--- regularity/utils/pretty.py
+++ regularity/utils/pretty.py
@@ -1,17 +1,21 @@
-from itertools import ifilter
+from itertools import ifilter, imap, izip
 from operator import itemgetter
 
-def unitize_value(value, unit):
+def unitize_value(value, unit, default=None):
     '''Transform a value into a string, appending a unit. If the value is Falsy,
-       then return None
+       then return the default value unitized.
 
        @param value : int|float|str
            the value to unitize
        @param unit : str
-           the unit (h, ms, kg, etc...)'''
+           the unit (h, ms, kg, etc...)
+       @param default : stringable
+           the value to use if value is falsy'''
     
-    if value:
-        return '%s%s' % (value, unit)
+    if not value:
+        value = default
+
+    return '%s %s' % (value, unit)
 
 def timedelta(td):
     '''Return a string representation of a duration.
@@ -27,30 +31,18 @@ def timedelta(td):
     seconds, remainder = divmod(remainder, 1)
     milliseconds = 1000 * round(remainder, 3)
 
-    days = unitize_value(days, 'd')
-    hours = unitize_value(hours, 'h')
-    minutes = unitize_value(minutes, 'm')
-    seconds = unitize_value(seconds, 's')
-    milliseconds = unitize_value(milliseconds, 'ms')
+    components = imap(int, (days, hours, minutes, seconds))
+    units = ('days', 'hours', 'minutes', 'seconds')
 
-    if days:
-        components = (days, hours, minutes)
+    components = izip(components, units)
+    components = ifilter(itemgetter(0), components)
+    components = list(unitize_value(c, u, 0) for c, u in components)
 
-    elif hours:
-        components = (hours, minutes, seconds)
+    if components:
+        return ', '.join(components)
 
-    elif minutes:
-        components = (minutes, seconds)
+    if milliseconds:
+        return '%d milliseconds' % milliseconds
 
-    elif seconds:
-        components = (seconds,)
-
-    elif milliseconds:
-        components = (milliseconds,)
-
-    else:
-        components = ('-',)
-
-    return ''.join(components)
-
-
+    return '-'
+        
